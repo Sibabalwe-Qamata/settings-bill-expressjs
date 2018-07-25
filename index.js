7e1e399fef3dfbdcf837ab1880d18c1ad673b5af
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-07-25 16:39:15 +0200

    added a form section for the view in html

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let express = require('express');
 let app = express();
 
 //route to redirect to a GET route
-res.redirect('/target-route');
+//res.redirect('/target-route');
 
 
 //Handlebars Section ...
@@ -22,6 +22,17 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 
+//Body parser ...
+
+let bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 //--------------------------Routes -------------------------------------------//
 
 app.get("/", function(req, res){
@@ -29,8 +40,24 @@ app.get("/", function(req, res){
   });
   
 //set the settings - sms & call price and the warning & critical level
-app.post('settings', function(){
+app.post('/settings', function(){
+    let smsCost = req.body.smsCost;
+    let callCost = req.body.callCost;
+    let warningLevel = req.body.warningLevel;
+    let criticalLevel = req.body.criticalLevel;
 
+    let settings = {
+      smsCost,
+      callCost,
+      warningLevel,
+      criticalLevel
+    };
+
+    // process data
+    globalSetings = settings;
+
+    // note that data can be sent to the template
+    res.render('home', {settings})
 
 
 
@@ -38,7 +65,9 @@ app.post('settings', function(){
 
 /**record an action of sms or call and the appropriate price based on the settings entered 
 & a timestamp when record has been entered.**/
-app.post('/action', function(){
+app.post('/action', function(req, res)
+{
+
 
 
 
@@ -46,7 +75,7 @@ app.post('/action', function(){
 
 
 //show all the actions - display the timestamps using fromNow and display a total cost for all the actions on the screen
-app.post('/actions', function(){
+app.post('/actions', function(req, res){
 
 
 
@@ -55,9 +84,21 @@ app.post('/actions', function(){
 //display all the sms or call actions - display the timestamps using fromNow and display a total cost for the selected action.
 
 app.get('/actions/:type', function (req, res) {
-    
-    
+    let costType = req.params.costType;
 
+    let cost = 0;
+    //lookup cost for costType
+    if (costType === 'sms'){
+        cost = settings.smsCost;
+    } else if (costType === 'call') {
+        cost = settings.callCost;
+    }
+
+    req.render('cost', {
+        costType,
+        cost
+    });
+    
 
 });
 
