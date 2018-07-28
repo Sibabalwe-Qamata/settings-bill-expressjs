e0fe07973e4ba15a0651f0cdabbaaa9774411658
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-07-28 12:32:10 +0200

    Added tests for the color function

--- test/settingBill.test.js
+++ test/settingBill.test.js
@@ -14,4 +14,18 @@ describe('Testing SettingsBill', function(){
         assert.equal(SettingBill.sumCall(), 2.75);
     } )
 
+    it('Should return warning or danger ', function(){
+
+        SettingBill.calls(2.75);
+        SettingBill.sms(0.75);
+        SettingBill.critical(3.00);
+        SettingBill.warning(5.75);
+        SettingBill.sumBill('call');
+        SettingBill.sumBill('sms');
+        SettingBill.colors(SettingBill.sumTotal());
+
+        assert.equal(SettingBill.colors(), 'warning');
+    } )
+
+
 })
